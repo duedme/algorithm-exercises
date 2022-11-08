cc96e6761ca410cd594775a76ab98c179e15fa7d
Daniel <duedme@gmail.com>
2022-11-08 11:57:51 -0600

    A solution of o(n) complexity was written.

--- p1/src/main.rs
+++ p1/src/main.rs
@@ -7,13 +7,16 @@ impl Solution  {
         let mut used: HashMap<i32, i32> = HashMap::new();
         let mut answer: Vec<i32> = Vec::new();
 
-        HashMap::insert(&mut used, nums[0], 0);
         for (index, num) in nums.iter().enumerate() {
+
                 if used.contains_key(&(target - num)) { 
                     answer.push(index as i32);
-                    answer.push(*(used.get(&(target - num)).unwrap()));   
-                    
+                    answer.push(*used.get(&(target - num)).unwrap());
+
                     return answer
+                    
+                } else {
+                    used.insert(*num, index as i32);
                 }
         }
 
@@ -22,5 +25,5 @@ impl Solution  {
 }
 
 fn main() {
-    assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+    assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![2, 1]);
 }
