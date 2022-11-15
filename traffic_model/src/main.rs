7fc9624dd4cf317c7c19d4c155466310b7d5547c
Daniel <duedme@gmail.com>
2022-11-15 11:06:35 -0600

    Starting main implementation for program.

--- traffic_model/src/main.rs
+++ traffic_model/src/main.rs
@@ -1,3 +1,11 @@
+use traffic_model::road::*;
+//use std::time::Duration;
+
 fn main() {
-    
+    let mut road: Road = Road::new();
+
+    println!("{}\n\n\n", Road::generate_printing_road(&road));
+    Road::mutate_road(&mut road );
+    println!("{}", Road::generate_printing_road(&road));
+
 }
