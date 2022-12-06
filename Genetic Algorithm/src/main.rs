ba730f16759a89c4fad9883d64892fa7ea1ae9fc
Daniel <duedme@gmail.com>
2022-12-06 14:17:23 -0600

    Started a genetic algorithm to find roots of an equation.

--- Genetic Algorithm/src/main.rs
+++ Genetic Algorithm/src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 extern crate rand;
 use rand::Rng;
+use std::collections::HashMap;
 // Generar población
 // Evaluar Función Objectivo
 // Criterio de término (si no, hay que hacer selección y cruzar individuos)
@@ -16,11 +17,37 @@ fn main() {
     let xmax = 10;
     let population: Vec<i32> = random_population_generator(xmin, xmax, num_of_candidates);
 
-    let population_results: Vec<f64> = create_population_results(&population, &num_of_candidates);
+    // Aptitud de la población.
+    let population_results: Vec<f64> = evaluation(&population, &num_of_candidates);
+    // Los mejores candidatos.
+    let lowest_results = picking(&population_results);
+    println!("Population: {:?}, \nPopulaton_results: {:?}, \nLowest_results: {:?}", population, population_results, lowest_results);
+
+}
+
+fn picking(population_results: &Vec<f64>) -> HashMap<usize, f64> {
+    let mut selection = HashMap::new();
+    let number_of_selected = 3;
+
+    let mut i = 1000;
+    let mut lowest = (population_results[0]).abs();
+    let mut iterations = 0;
+
+    while iterations != number_of_selected {
+
+        for (index, number) in population_results.iter().enumerate() {
+            if selection.get(&index).is_some() { continue; };
+            if number.abs() < lowest.abs() { i = index; lowest = *number; };
+            selection.insert(i, lowest);
+        }
+        iterations += 1;
+    }
+
+    selection
 }
 
 // Aquí va la función
-fn create_population_results(population: &Vec<i32>, num_of_candidates: &i32) -> Vec<f64> {
+fn evaluation(population: &Vec<i32>, num_of_candidates: &i32) -> Vec<f64> {
     let mut pop: Vec<f64> = Vec::new();
 
         for i in 0..*num_of_candidates {
