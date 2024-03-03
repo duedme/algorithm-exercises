9f1eb0f87f67d03e79f09b7a839d7fa694c648ea
Daniel Espejel <duedme@gmail.com>
2024-03-02 21:37:02 -0600

    Added "Diagonal Difference"

---
+++ Diagonal Difference/src/main.rs
@@ -0,0 +1,44 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, Write};
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+fn diagonalDifference(arr: &[Vec<i32>]) -> i32 {
+    let arr_len = arr.iter().count();
+    let normal = (0..arr_len).map(|el| arr[el][el]).sum::<i32>();
+    let rev = (0..arr_len).map(|el| arr[el][(arr_len - 1) - el]).sum::<i32>();
+
+    let sum = if normal - rev > 0 { normal - rev } else { rev - normal };
+
+    sum
+}
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let mut fptr = File::create(env::var("OUTPUT_PATH").unwrap()).unwrap();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    let mut arr: Vec<Vec<i32>> = Vec::with_capacity(n as usize);
+
+    for i in 0..n as usize {
+        arr.push(Vec::with_capacity(n as usize));
+
+        arr[i] = stdin_iterator.next().unwrap().unwrap()
+            .trim_end()
+            .split(' ')
+            .map(|s| s.to_string().parse::<i32>().unwrap())
+            .collect();
+    }
+
+    let result = diagonalDifference(&arr);
+
+    writeln!(&mut fptr, "{}", result).ok();
+}
